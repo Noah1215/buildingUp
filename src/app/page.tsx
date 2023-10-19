91206b9fd941986fa3be128bddb70a1c9e03ca42
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-10-19 11:41:05 -0400

    signIn_Initial_commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,119 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import Drawer from '@mui/material/Drawer';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import MediaCard from '@/components/MediaCard';
+"use client";
+import React from "react";
+import {
+  Box,
+  Typography,
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
 
 export default function HomePage() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <div>
-        <Alert severity="info" sx={{ mt: 2, mb: 5 }}>
-          <AlertTitle>Hello 👋</AlertTitle>
-          This app uses the Next.js App Router and Material UI v5.
-        </Alert>
-        <Grid container rowSpacing={3} columnSpacing={3}>
-          <Grid xs={6}>
-            <MediaCard
-              heading="CMYK"
-              text="The CMYK color model (also known as process color, or four color) is a subtractive color model, based on the CMY color model, used in color printing, and is also used to describe the printing process itself."
-            />
-          </Grid>
-          <Grid xs={6}>
-            <MediaCard
-              heading="HSL and HSV"
-              text="HSL (for hue, saturation, lightness) and HSV (for hue, saturation, value; also known as HSB, for hue, saturation, brightness) are alternative representations of the RGB color model, designed in the 1970s by computer graphics researchers."
-            />
-          </Grid>
-          <Grid xs={6}>
-            <MediaCard
-              heading="RGB"
-              text="An RGB color space is any additive color space based on the RGB color model. RGB color spaces are commonly found describing the input signal to display devices such as television screens and computer monitors."
-            />
-          </Grid>
-          <Grid xs={6}>
-            <MediaCard
-              heading="CIELAB"
-              text="The CIELAB color space, also referred to as L*a*b*, was intended as a perceptually uniform space, where a given numerical change corresponds to a similar perceived change in color."
-            />
-          </Grid>
-        </Grid>
-      </div>
-      <Drawer
+    <Box height="100vh" display="flex">
+      <Box
         sx={{
-          width: 320,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: 320,
-            boxSizing: 'border-box',
-            top: ['48px', '56px', '64px'],
-            height: 'auto',
-            bottom: 0,
-          },
+          flex: 6,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
-        variant="permanent"
-        anchor="right"
       >
-        <List sx={{ px: 2 }}>
-          <ListItem disablePadding>
-            <Typography variant="overline" sx={{ fontWeight: 500 }}>
-              On this page
-            </Typography>
-          </ListItem>
-        </List>
-      </Drawer>
+        <img src="/img/logo.png" alt="logo" width="50%" />
+      </Box>
+      <Box sx={{ flex: 4, backgroundColor: "#024761" }}>
+        <div
+          style={{
+            width: "80%",
+            margin: "5rem 4rem",
+          }}
+        >
+          <Typography
+            variant="h4"
+            sx={{ color: "white", marginBottom: "5rem" }}
+          >
+            Welcome!
+          </Typography>
+
+          <form style={{ display: "flex", flexDirection: "column" }}>
+            <TextField
+              id="filled-basic__email"
+              label="Email"
+              variant="filled"
+              margin="normal"
+              color="warning"
+              sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
+            />
+            <TextField
+              id="filled-basic__password"
+              label="Password"
+              variant="filled"
+              margin="normal"
+              color="warning"
+              sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
+            />
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <FormControlLabel
+                control={<Checkbox color="warning" sx={{ color: "white" }} />}
+                label={<span style={{ color: "white" }}>Remember me</span>}
+              />
+
+              <a href="/findPassword" style={{ textDecoration: "none" }}>
+                <Typography
+                  sx={{
+                    color: "#727272",
+                    "&:hover": {
+                      color: "white",
+                      cursor: "pointer",
+                    },
+                  }}
+                >
+                  Forgot the password?
+                </Typography>
+              </a>
+            </div>
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="warning"
+              style={{ margin: "3rem 0 6rem 0" }}
+              href="/home"
+              fullWidth
+              sx={{ height: "3.5rem" }}
+            >
+              Login
+            </Button>
+          </form>
+          <div
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <a href="/register" style={{ textDecoration: "none" }}>
+              <Typography
+                sx={{
+                  color: "#727272",
+                  "&:hover": {
+                    color: "white",
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                Don't have an account? Register
+              </Typography>
+            </a>
+          </div>
+        </div>
+      </Box>
     </Box>
   );
 }
