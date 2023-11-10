c6afa2b7929f2e04e6910c4c4718baf16769d6ee
Lucas <lucasywlee@gmail.com>
2023-11-10 14:14:42 -0500

    remove comments in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,32 +4,5 @@ import { createClient } from "@/lib/supabase/middleware";
 export async function middleware(request: NextRequest) {
   const { supabase, response } = createClient(request);
   await supabase.auth.getSession();
-
-  /*
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user && request.nextUrl.pathname !== "/") {
-    console.log("User is not authenticated. Redirecting to /");
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-
-  const { data } = await supabase
-    .from("user_roles")
-    .select("role")
-    .eq("user_id", user?.id)
-    .single();
-  const userRole = data?.role;
-
-  if (user && !request.nextUrl.pathname.startsWith(`/${userRole}`)) {
-    console.log(
-      `Path ${request.nextUrl.pathname} is not starting with /${userRole}, redirecting to /${userRole}`
-    );
-    return NextResponse.redirect(new URL(`${userRole}`, request.url));
-  }
-  */
-
   return response;
 }
