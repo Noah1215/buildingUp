fd7beeaad3da9ba3ad39027d197232b967fc3607
Lucas <lucasywlee@gmail.com>
2023-11-10 00:25:45 -0500

    fix typo in redirect url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { createClient } from "@/lib/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
-  const { supabase, response } = createClient(request);
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user && request.nextUrl.pathname !== "/") {
-    console.log("user is not authenticated. redirecting to /");
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  const { data } = await supabase
-    .from("user_roles")
-    .select("role")
-    .eq("user_id", user?.id)
-    .single();
-  const userRole = data?.role;
-
-  if (user && request.nextUrl.pathname.startsWith(`/${userRole}}`)) {
-    return NextResponse.redirect(new URL(`${userRole}}`, request.url));
-  }
-
-  // Refresh session if expired - required for Server Components
-  await supabase.auth.getSession();
-
-  return response;
-}
-
 export const config = {
   matcher: [
     /*
@@ -42,3 +13,32 @@ export const config = {
     "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
+
+export async function middleware(request: NextRequest) {
+  const { supabase, response } = createClient(request);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user && request.nextUrl.pathname !== "/") {
+    console.log("User is not authenticated. Redirecting to /");
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  const { data } = await supabase
+    .from("user_roles")
+    .select("role")
+    .eq("user_id", user?.id)
+    .single();
+  const userRole = data?.role;
+
+  if (user && !request.nextUrl.pathname.startsWith(`/${userRole}`)) {
+    console.log(
+      `Path ${request.nextUrl.pathname} is not starting with /${userRole}, redirecting to /${userRole}`
+    );
+    return NextResponse.redirect(new URL(`${userRole}`, request.url));
+  }
+
+  return response;
+}
