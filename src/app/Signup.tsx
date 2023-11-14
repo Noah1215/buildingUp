34a020978ffc2abd0d41e26d5e3a51a44bfa93fe
Jay <77955605+LimJunBeom@users.noreply.github.com>
2023-11-14 00:46:13 -0500

    Deleted 'react-hook-form'
    Deleted 'react hook form' used for form validation and merge 'develop' into 'front-validation'

--- src/app/Signup.tsx
+++ src/app/Signup.tsx
@@ -9,58 +9,91 @@ import FormControlLabel from "@mui/material/FormControlLabel/FormControlLabel";
 import TextField from "@mui/material/TextField/TextField";
 import Typography from "@mui/material/Typography/Typography";
 
-import { useForm, SubmitHandler } from "react-hook-form";
-import { DevTool } from "@hookform/devtools";
-import Stack from "@mui/material/Stack/Stack";
+type Status = "Mobile" | "Desktop";
 
-type FormValues = {
-  email: string;
-  password: string;
-};
-
-export const Signup = () => {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-    control,
-  } = useForm<FormValues>({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const onSubmit = (data: FormValues) => {
-    console.log(data);
-  };
-
-  return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <Stack spacing={2} width={400}>
-          <TextField
-            label="Email"
-            type="email"
-            {...register("email", { required: "Email is required" })}
-            error={!!errors.email}
-            helperText={errors.email?.message}
-          />
-          <TextField
-            label="Password"
-            type="password"
-            {...register("password", { required: "Password is required" })}
-            error={!!errors.password}
-            helperText={errors.password?.message}
-          />
-          <Button type="submit" variant="contained" color="primary">
-            Login
-          </Button>
-        </Stack>
+const Signup = (props: { device: Status }) => {
+  if (props.device === "Mobile") {
+    return (
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <TextField
+          id="outlined-basic__email"
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          color="warning"
+          sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
+        />
+        <TextField
+          id="outlined-basic__password"
+          label="Password"
+          variant="outlined"
+          margin="normal"
+          color="warning"
+          sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
+        />
       </form>
-      <DevTool control={control} />
-    </>
-  );
+    );
+  } else if (props.device === "Desktop") {
+    return (
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <TextField
+          id="filled-basic__email"
+          label="Email"
+          variant="filled"
+          margin="normal"
+          color="warning"
+          sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
+        />
+        <TextField
+          id="filled-basic__password"
+          label="Password"
+          variant="filled"
+          margin="normal"
+          color="warning"
+          sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
+        />
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+          component="footer"
+        >
+          <FormControlLabel
+            control={<Checkbox color="warning" sx={{ color: "white" }} />}
+            label={<span style={{ color: "white" }}>Remember me</span>}
+          />
+
+          <Link href="/findPassword" style={{ textDecoration: "none" }}>
+            <Typography
+              sx={{
+                color: "#727272",
+                "&:hover": {
+                  color: "white",
+                  cursor: "pointer",
+                },
+              }}
+            >
+              Forgot the password?
+            </Typography>
+          </Link>
+        </Box>
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="warning"
+          href="/"
+          fullWidth
+          sx={{ height: "3.5rem", margin: "3rem 0 6rem 0" }}
+        >
+          Login
+        </Button>
+      </form>
+    );
+  }
 };
 
 export default Signup;
