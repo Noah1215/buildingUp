e8a51995d963f670f54dd0264adc555d3a823ec5
Lucas <lucasywlee@gmail.com>
2023-11-26 18:26:48 -0500

    update routes

--- src/app/alumni/events/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default async function Events() {
-    return (
-        <div>
-            <h1>Events</h1>
-        </div>
-    );
-}
--- src/app/alumni/supports/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default async function Supports() {
-    return (
-        <div>
-            <h1>Supports</h1>
-        </div>
-    );
-}
