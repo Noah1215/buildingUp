15d195c9cfbf835698ded57afd3d7f4d5f8f2b8e
Lucas <lucasywlee@gmail.com>
2023-11-26 18:28:21 -0500

    match breakpoints with other components

--- src/app/alumni/page.tsx
+++ src/app/alumni/page.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-
+// Component
+import Board from "@/app/alumni/board";
+// MUI
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
 
-import Board from "@/app/alumni/board";
-
+// TODO: replace sample data
 const announcements = [
   { title: "[Workshop] Alumni Workshop", date: "2023-11-28" },
   { title: "[Party] Trainee Party", date: "2023-10-24" },
 ];
-
+// TODO: replace sample data
 const Schedule = [
   { title: "Mentor 1", date: "2023-11-23" },
   { title: "Mentor 2", date: "2023-10-25" },
@@ -19,10 +19,10 @@ export default async function Home() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={5}>
-        <Grid xs={12} sm={6}>
+        <Grid xs={12} md={6}>
           <Board title={"Announcement"} data={announcements} />
         </Grid>
-        <Grid xs={12} sm={6}>
+        <Grid xs={12} md={6}>
           <Board title={"Schedule"} data={Schedule} />
         </Grid>
       </Grid>
