4d5c25993394d7f5d76730073e64b64c3434cb70
Lucas <lucasywlee@gmail.com>
2023-12-02 14:21:11 -0500

    add middleware log

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { type NextRequest } from "next/server";
 import { createClient } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+  console.log(`${new Date(Date.now())} middleware ${request.url}`);
   const { supabase, response } = createClient(request);
   await supabase.auth.getSession();
   return response;
