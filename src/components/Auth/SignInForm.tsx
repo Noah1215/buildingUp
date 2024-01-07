3c8d7a50e524c619c4ef922407b7c4f134dd17fb
Lucas <lucasywlee@gmail.com>
2024-01-07 00:01:06 -0500

    remove href attribute and add onSubmit handler

--- src/components/Auth/SignInForm.tsx
+++ src/components/Auth/SignInForm.tsx
@@ -39,13 +39,13 @@ const SignInForm = (props: { device: Status }) => {
   const handleMouseDownPassword = (
     event:
       | React.MouseEvent<HTMLButtonElement>
-      | React.MouseEvent<HTMLDivElement>,
+      | React.MouseEvent<HTMLDivElement>
   ) => {
     event.preventDefault();
   };
 
   const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarMessage, setSnackbarMessage] = useState("");
 
   const handleSignIn = async () => {
     const supabase = createClient();
@@ -87,75 +87,81 @@ const SignInForm = (props: { device: Status }) => {
 
   const handleCloseSnackbar = () => {
     setSnackbarOpen(false);
-  }
+  };
 
-  const handleFormSubmit = (e:any) => {
+  const handleFormSubmit = (e: any) => {
     e.preventDefault();
     handleSignIn();
-  }
+  };
 
-  const handleKeyPress = (e:any) => {
-    if( e.key === 'Enter' ) {
+  const handleKeyPress = (e: any) => {
+    if (e.key === "Enter") {
       handleSignIn();
     }
-  }
+  };
 
-  const onEmailHandler = (e:any) => {
+  const onEmailHandler = (e: any) => {
     setEmail(e.target.value);
-  }
-  const onPasswordHandler = (e:any) => {
+  };
+  const onPasswordHandler = (e: any) => {
     setPassword(e.target.value);
-  }
+  };
 
   const EmailValidation = () => {
-    let check = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    if( email == '' ) {
+    let check =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (email == "") {
       return false;
-    } else{
-      return !(check.test(email));
+    } else {
+      return !check.test(email);
     }
-  }
+  };
 
   const PasswordValidation = () => {
     //let check = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
     let check = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
-    if( password == '' ) {
+    if (password == "") {
       return false;
     } else {
-      return !(check.test(password));
+      return !check.test(password);
     }
-  }
+  };
 
   if (props.device === "Mobile") {
     return (
-      <form style={{ display: "flex", flexDirection: "column" }}>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleFormSubmit}
+      >
         <TextField
           id="outlined-basic__email"
           label="Email"
-          onChange={ onEmailHandler }
+          onChange={onEmailHandler}
           value={email}
           variant="outlined"
           margin="normal"
           color="warning"
           sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
-
-          error={ EmailValidation() }
-          helperText={ EmailValidation() ? "Invalid email format." : "" }
+          error={EmailValidation()}
+          helperText={EmailValidation() ? "Invalid email format." : ""}
         />
         <TextField
           id="outlined-basic__password"
           label="Password"
           type={showPassword ? "text" : "password"}
-          onChange={ onPasswordHandler }
+          onChange={onPasswordHandler}
           value={password}
           variant="outlined"
           margin="normal"
           color="warning"
           sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
-
-          error={ PasswordValidation() }
+          error={PasswordValidation()}
           //helperText={ PasswordValidation() ? "Your password must have at least 8 letters, numbers and symbols (such as ! and %)." : ""}
-          helperText={ PasswordValidation() ? "Your password must have at least 8 letters and numbers." : ""}
+          helperText={
+            PasswordValidation()
+              ? "Your password must have at least 8 letters and numbers."
+              : ""
+          }
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -191,15 +197,22 @@ const SignInForm = (props: { device: Status }) => {
           variant="contained"
           color="warning"
           style={{ margin: "3rem 0 6rem 0" }}
-          href="/"
           fullWidth
           sx={{ height: "3.5rem" }}
         >
           Login
         </Button>
-        <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleCloseSnackbar}>
-          <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: "100%" }}>
-            {snackbarMessage}            
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={6000}
+          onClose={handleCloseSnackbar}
+        >
+          <Alert
+            onClose={handleCloseSnackbar}
+            severity="error"
+            sx={{ width: "100%" }}
+          >
+            {snackbarMessage}
           </Alert>
         </Snackbar>
       </form>
@@ -207,31 +220,35 @@ const SignInForm = (props: { device: Status }) => {
   }
 
   return (
-    <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleFormSubmit}>
+    <form
+      style={{ display: "flex", flexDirection: "column" }}
+      onSubmit={handleFormSubmit}
+    >
       <TextField
         id="filled-basic__email"
         label="Email"
-        onChange={ onEmailHandler }
+        onChange={onEmailHandler}
         onKeyDown={handleKeyPress}
         value={email}
         variant="filled"
         margin="normal"
         color="warning"
         sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
-
-        error={ EmailValidation() }
-        helperText={ EmailValidation() ? "Invalid email format." : "" }
-        FormHelperTextProps={{ style: { 
-          color: "orange",
-          backgroundColor: '#024761',
-          margin: 0,
-          paddingLeft: 15
-        }}}
+        error={EmailValidation()}
+        helperText={EmailValidation() ? "Invalid email format." : ""}
+        FormHelperTextProps={{
+          style: {
+            color: "orange",
+            backgroundColor: "#024761",
+            margin: 0,
+            paddingLeft: 15,
+          },
+        }}
       />
       <TextField
         id="filled-basic__password"
         label="Password"
-        onChange={ onPasswordHandler }
+        onChange={onPasswordHandler}
         onKeyDown={handleKeyPress}
         type={showPassword ? "text" : "password"}
         value={password}
@@ -239,16 +256,21 @@ const SignInForm = (props: { device: Status }) => {
         margin="normal"
         color="warning"
         sx={{ backgroundColor: "white", borderRadius: "0.2rem" }}
-
-        error={ PasswordValidation() }
+        error={PasswordValidation()}
         //helperText={ PasswordValidation() ? "Your password must have at least 8 letters, numbers and symbols (such as ! and %)." : ""}
-        helperText={ PasswordValidation() ? "Your password must have at least 8 letters and numbers." : ""}
-        FormHelperTextProps={{ style: {
-          color: "orange", 
-          backgroundColor: '#024761',
-          margin: 0,
-          paddingLeft: 15
-        }}}
+        helperText={
+          PasswordValidation()
+            ? "Your password must have at least 8 letters and numbers."
+            : ""
+        }
+        FormHelperTextProps={{
+          style: {
+            color: "orange",
+            backgroundColor: "#024761",
+            margin: 0,
+            paddingLeft: 15,
+          },
+        }}
         InputProps={{
           endAdornment: (
             <InputAdornment position="end">
@@ -302,14 +324,18 @@ const SignInForm = (props: { device: Status }) => {
       >
         Login
       </Button>
-      <Snackbar 
-        open={snackbarOpen} 
-        autoHideDuration={10000} 
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={10000}
         onClose={handleCloseSnackbar}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
       >
-        <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: "100%" }}>
-          "Fail Login. Check your ID and Password"           
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity="error"
+          sx={{ width: "100%" }}
+        >
+          "Fail Login. Check your ID and Password"
         </Alert>
       </Snackbar>
     </form>
