fd67f445da745a057fc61fab93a2235d55a8eabc
Lucas <lucasywlee@gmail.com>
2024-01-23 01:56:18 -0500

    close menu when navigate to other routes

--- src/app/alumni/components/layout/BottomNavigation.tsx
+++ src/app/alumni/components/layout/BottomNavigation.tsx
@@ -145,6 +145,7 @@ export default function BottomNavigation() {
           <StyledMenuItem
             key={item.label}
             onClick={() => {
+              handleClose();
               router.push(item.link);
             }}
           >
